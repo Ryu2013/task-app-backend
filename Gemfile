b88f6986ef61fc2faf6902aab7871ec54a303882
ryu <kaigo2013.ryuuiti@gmail.com>
2026-01-03 16:44:02 +0900

    ci: ciのデプロイ先のディレクトリパスがEC2とズレていたのを直す

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem "thruster", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 gem "image_processing", "~> 1.2"
 gem "aws-sdk-s3", require: false
-gem 'rack-cors'
+gem "rack-cors"
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin Ajax possible
 # gem "rack-cors"
 
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # CloudFrontのドメイン（例: https://d12345.cloudfront.net）を指定
-    origins 'http://localhost:3000', 'http://d39ivsn50b0rlf.cloudfront.net', 'https://d39ivsn50b0rlf.cloudfront.net'
+    origins "http://localhost:3000", "http://d39ivsn50b0rlf.cloudfront.net", "https://d39ivsn50b0rlf.cloudfront.net"
 
-    resource '*',
+    resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ]
   end
 end
